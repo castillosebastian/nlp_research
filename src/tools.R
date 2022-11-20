3206d9675f3aba288e77bcb0715f167a52093c1c
Sebastian Castillo <castilloclaudiosebastian@gmail.com>
2022-11-20 20:18:06 -0300

    src

--- src/tools.R
+++
@@ -1,6 +0,0 @@
-
-
-# modelo de lenguaje preentrenado españo-legal
-# https://github.com/PlanTL-GOB-ES/lm-legal-es
-# https://huggingface.co/PlanTL-GOB-ES/RoBERTalex?text=La+ley+fue+%3Cmask%3E+finalmente.
-
---
+++ src/workflow_plan_resources.R
@@ -0,0 +1,17 @@
+# Codigo disponible para cada etapa del proyecto
+
+# Preprocesamiento----
+# 1 )
+# En la carpeta BLSTM_CNN_Francesca: codigo para el preprocesmiento e ingenierìa de features
+# con implementación de modelos BLSTM-CNN y Modelo de Lenguaje BETO.
+
+# 2) 
+# En la carpeta Legal-Entity-Recognition-master BLSTM_CRF con leitner: código para leitner para pipeline completo de implementaciòn
+
+# 3) 
+# En la carpeta mrc-for-flat-nested-ner-master Transformer con li2020b: código trasnformer
+
+
+# modelo de lenguaje preentrenado españo-legal
+# https://github.com/PlanTL-GOB-ES/lm-legal-es
+# https://huggingface.co/PlanTL-GOB-ES/RoBERTalex?text=La+ley+fue+%3Cmask%3E+finalmente.
