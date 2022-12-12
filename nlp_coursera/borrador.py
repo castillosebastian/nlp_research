e38a43a3f73a3bbe572eee07e7df338cb5998307
sebacastillo <castilloclaudiosebastian@gmail.com>
2022-12-12 20:52:59 -0300

    w2

--- nlp_coursera/borrador.py
+++ nlp_coursera/borrador.py
@@ -1,11 +1,14 @@
 
-
+#%%
 result = {}
-tweets = ['i am happy', 'i am tricked', 'i am sad', 'i am tired', 'i am tired']
-ys = [1, 0, 0, 0, 0]
-count_tweets(result, tweets, ys)
+tweets = ['i am happy', 'i am tricked', 'i am sad', 'i am tired', 'i am tired', 'i am big happy']
+ys = [1, 0, 0, 0, 0, 1]
+for y, tweet in zip(ys, tweets):
+    print(y, tweet)
+process_tweet(tweets[5])
 
 
+# %%
 def count_tweets(result, tweets, ys):
     '''
     Input:
@@ -19,15 +22,18 @@ def count_tweets(result, tweets, ys):
     for y, tweet in zip(ys, tweets):
         for word in process_tweet(tweet):
             # define the key, which is the word and label tuple
-            pair = None
+            pair = (word, y)
             
             # if the key exists in the dictionary, increment the count
             if pair in result:
-                result[pair] += None
+                result[pair] += 1 
 
             # else, if the key is new, add it to the dictionary and set the count to 1
             else:
-                result[pair] = None
+                result[pair] = 1
     ### END CODE HERE ###
 
     return result
+
+count_tweets(result, tweets, ys)
+result[("happi", 1)]
